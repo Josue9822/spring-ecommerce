aff774d760f3da583827685a2d1484bfc87f8d8d
Josue Perez <jos22_98@hotmail.com>
2025-04-22 12:19:56 -0500

    validar datos repetidos de un producto en el carrito y ver el carrito desde la cabecera del template usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -75,8 +75,15 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//Validar que el producto no se agregue dos veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
 		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+		
+				
 		sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
 		orden.setTotal(sumaTotal);
@@ -110,6 +117,15 @@ public class HomeController {
 		model.addAttribute("orden", orden);
 		
 		return "usuario/carrito";
+	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "/usuario/carrito";
 	}
 	
 
